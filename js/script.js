62b5f8066452daaf8ec77bbdf27b252870d90421
Mufasa <mustafaadam1902@gmail.com>
2018-03-27 18:33:50 +0400

    Feature Swipeable Tabs

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 // Initializes Side Navigation
 $(".button-collapse").sideNav();
+
+// Initializes Tabs + Swipeable effect
+$(document).ready(function(){
+   $('ul.tabs').tabs({'swipeable': true});
+ });
